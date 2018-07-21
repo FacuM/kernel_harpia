e8c4cc0daa8d146222dbf97e482d0584eccae7f8
Facundo Montero <facumo.fm@gmail.com>
2018-07-21 04:52:52 -0300

    harpharpia.sh > build.sh, make it support all variants

--- build.sh
+++ build.sh
@@ -3,14 +3,10 @@ Anykernel_DIR=$KERNEL_DIR/Anykernel2/harpia
 TOOLCHAINDIR=$(pwd)/toolchain/linaro-7.2
 DATE=$(date +"%d%m%Y")
 KERNEL_NAME="BLEEDING_EDGE-Kernel"
-DEVICE="-harpia-"
-VER="-v70"
-TYPE="-OREO"
-FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
 
 export ARCH=arm
-export KBUILD_BUILD_USER="Aki"
-export KBUILD_BUILD_HOST="SEND_NUDES__PLEASE"
+export KBUILD_BUILD_USER="$3"
+# export KBUILD_BUILD_HOST="SEND_NUDES__PLEASE"
 export CROSS_COMPILE=$TOOLCHAINDIR/bin/arm-eabi-
 export USE_CCACHE=1
 
@@ -19,16 +15,24 @@ then
 rm arch/arm/boot/zImage #Just to make sure it doesn't make flashable zip with previous zImage
 fi;
 
-if [ -z "$1" ]
+if [ -z $2 ] || [ -z $3 ]
+then
+ printf "\nUsage: \n\n\tbash build.sh [thread_amount] device_codename maintainer_username\n\n\tNOTE: '[thread_amount]' can be an integer or 'auto'.\n\n"
+ exit 1
+fi
+DEVICE="-$2-"
+VER="-v70"
+TYPE="-OREO"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
+if [ "$1" == 'auto' ]
 then
  t=$(nproc --all)
 else
  t=$1
 fi
-
-echo "Will now run using $t threads."
+printf "\nTHREADS: $t\nDEVICE: $2\nMAINTAINER: $3\n\n"
 echo "Making kernel binary"
-make harpia_defconfig
+make $2_defconfig
 make -j$( nproc --all ) zImage
 
 if [ -e  arch/arm/boot/zImage ];
